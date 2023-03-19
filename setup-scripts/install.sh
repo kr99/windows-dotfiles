cfef8017d97360b4929161e7e6a12c5318872d7b
Kimball Robinson <kimball.robinson@chghealthcare.com>
2023-03-18 23:29:31 -0600

    install script updates

--- setup-scripts/install.sh
+++ setup-scripts/install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+#
 
-# got some of this from stackoverflow, others.
-
-if type curl >/dev/null 2>&1
-then 
-	bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-else
-	echo "curl not installed - aborting"
-	exit 0
+net session > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+	echo "OK, running as admin"
+else 
+	echo "Please run this script as administrator"
+	exit
 fi
 
-export MSYS=winsymlinks:nativestrict
-
+if type choco
+then
+	choco install --yes wget curl ditto freefilesync notepadplusplus
+	choco install --yes git gow nvm vim
+	choco install --yes openssh
+	echo "Now, use freefilesync to pull AppData and other config files out of OneDrive/(employer)"
+else
+	echo "no chocolatey found"
+	exit
+fi
 
 echo "Installing sdkman (which helps manage java versions)."
 
@@ -25,7 +32,38 @@ sdk install maven   #install latest as default
 sdk env install #install java version specified in .sdkmanrc in this project
 sdk env #switch to the java version noted in .sdkmanrc
 
+# got some of this from stackoverflow, others.
+shopt -s nocasematch
+if [[ $USERNAME =~ kiroBINS ]]
+then
+	echo "setting git username and email"
+	git config --global user.name "Kimball Robinson"
+	git config --global user.email "kimball.robinson@chghealthcare.com"
+else
+	echo "username not found"
+fi
 
+if type curl >/dev/null 2>&1
+then 
+	echo "Installing oh-my-bash last, since it tends to replace the bashrc"
+	echo "installing oh-my-bash"
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+else
+	echo "curl not installed - aborting"
+	exit 0
+fi
 
-echo "Installing oh-my-bash last, as it takes over"
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+export MSYS=winsymlinks:nativestrict
+
+echo "Running zscaler setup script" # must come after sdkman
+bash setup-scripts/setup-zscaler.bash
+
+choco install --yes powertoys
+choco install --yes autohotkey 
+choco install --yes wincompose
+choco install --yes postman powertoys slack 
+choco install --yes dbeaver jbs conemu 
+#choco install --yes intellijidea-ultimate # perhaps download it manually
+#choco install --yes Firefox #probably already installed by now, manually.
+# getting to fun/extras...
+choco install --yes vlc gimp
